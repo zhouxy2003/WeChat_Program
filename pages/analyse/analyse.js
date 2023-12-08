f1054b751c38a3f89ab400acbdba9a6d458d1456
kwkzuishuai <134843765+kwkzuishuai@users.noreply.github.com>
2023-12-08 12:53:24 +0800

    添加分析逻辑，通过id获取数据的云函数

--- pages/analyse/analyse.js
+++ pages/analyse/analyse.js
@@ -169,8 +169,20 @@ function initGaugeChart(canvas, width, height, dpr) {
   return chart;
 }
  
-Page({
+Page(
+{
   data: {
+    standard:{
+      agestandard:[22,23,26,32]
+    },
+    // 用户id，单人
+    userid : "",
+    // 用户得分列表
+    userscorelist :[],
+    // 储存用户数据的对象
+    dataOBJ:[{
+
+    }],
     // userName:"简历分析",
     ec: {
       onInit: function (canvas, width, height, dpr) {
@@ -194,11 +206,195 @@ Page({
         };
       }
     }
+      //简历数据存放区
+    ,
+  },
+ 
+ 
+
+
+ 
+ 
+  
+  analyes(education, salary, age, skill, timeDiff) {
+    
+  // 学历权重
+  const educationWeights = {
+    "专科": 5,
+    "本科": 10,
+    "硕士": 15,
+    "博士": 20
+  };
+
+  // 薪资得分
+  const salaryScore = 10;
+
+  // 年龄划分和权重
+  const ageRanges = {
+    "专科": 22,
+    "本科": 23,
+    "硕士": 26,
+    "博士": 32
+  };
+
+
+
+  // 技能得分
+  const skillScore = 10;
+
+  // 预计入职时间与当前时间差划分和权重
+
+
+    // 计算各部分得分
+    const educationScore = educationWeights[education];
+    const ageScore = this.calculateAgeScore(ageRanges[education], age);
+    const timeScore = this.calculateTimeScore(timeDiff);
+
+    // 计算综合得分
+    const totalScore = parseFloat((educationScore + salaryScore + ageScore + skillScore + timeScore))/ 5.0;
+    // console.log("学历得分" +educationScore)
+    // console.log("薪酬得分" +salaryScore)
+    // console.log("年龄得分" +ageScore)
+    // console.log("技能得分" +skillScore)
+    // console.log("时间得分" +timeScore)
+    // console.log(totalScore)
+
+    return totalScore;
+  },
+
+  // 根据年龄范围计算年龄得分
+  calculateAgeScore(baseAge, age) {
+    const ageDiff = age - baseAge;
+    const ageWeights = {
+      "x及x以下": 5,
+      "x—x+3": 10,
+      "x+3—x+6": 20,
+      "x+6以上": 15
+    };
+
+    if (ageDiff <= 0) {
+      return ageWeights["x及x以下"];
+    } else if (ageDiff <= 3) {
+      return ageWeights["x—x+3"];
+    } else if (ageDiff <= 6) {
+      return ageWeights["x+3—x+6"];
+    } else {
+      return ageWeights["x+6以上"];
+    }
+  },
+
+  // 根据时间差划分计算时间得分
+  calculateTimeScore(timeDiff) {
+    const timeRanges = {
+      "1个月": 20,
+      "1-3个月": 8,
+      "3个月以上": 1
+    };
+    if (timeDiff <= 30) {
+      return timeRanges["1个月"];
+    } else if (timeDiff <= 90) {
+      return timeRanges["1-3个月"];
+    } else {
+      return timeRanges["3个月以上"];
+    }
+  },
+
+  // // 示例使用
+  // const education = "本科";
+  // const salary = 10;
+  // const age = 25;
+  // const skill = 10;
+  // const timeDiff = 45; // 假设时间差为45天
+  
+  userInputAction: function (options) {
+    //获取输入框输入的内容
+    // console.log("获取文本框内容")
+    let value = options.detail.value;
+    // console.log(value);
+    // 将userid修改为传入的数据
+    this.setData({
+      userid : value
+    })
+    // console.log("数据修改了~"+this.data.userid)
+  },
+  gettest(){
+    var n = app.globalData.userArr
+    // console.log(n)   
+    var resumeScore = null
+    var templist = [] // 零时列表用来存储所有用户的得分
+    //循环获取所有用户的得分
+    for (let i = 0 ; i < n.length;i+=1){
+      var timeDiff = this.dateNum(n[i].jobTime);
+      resumeScore = this.analyes(n[i].degree, n[i].salary, n[i].age, n[i].features, timeDiff);
+      templist.push(resumeScore);
+      // console.log(templist)
+    }
+    this.setData({
+      userscorelist : templist.sort() //将所有用户的得分存入本页data中
+    })
+    // 获取单个用户的得分
+    var id = this.data.userid;
+    var timeDiff = this.dateNum(n[id].jobTime);
+    resumeScore = this.analyes(n[id].degree, n[id].salary, n[id].age, n[id].features, timeDiff);
+    var location = this.data.userscorelist.indexOf(resumeScore) //得分在列表中的位置
+    var length = this.data.userscorelist.length //列表总长度
+    var postion = (parseFloat(location)/parseFloat(length)).toFixed(2);
+    // console.log(postion)
+    //单个用户的具体数据
+    const educationWeights = {
+      "专科": 5,
+      "本科": 10,
+      "硕士": 15,
+      "博士": 20
+    };
+    const timeRanges = {
+      "1个月": 20,
+      "1-3个月": 8,
+      "3个月以上": 1
+    };
+    const ageRanges = {
+      "专科": 22,
+      "本科": 23,
+      "硕士": 26,
+      "博士": 32
+    };
+ 
+    var degreeScore = educationWeights[n[id].degree];
+    var timeScore = this.calculateTimeScore(timeDiff);
+    var featuresScore = 10
+    var ageScore = this.calculateAgeScore(ageRanges[n[id].degree], n[id].age)
+    var salaryScore = 10
+    timeDiff = this.dateNum(n[id].jobTime);
+    
+    console.log([degreeScore*50,timeScore*50,featuresScore*50,ageScore*50,salaryScore*50]+"    " + postion)
+    // 输出综合得分
+    // return postion, [degreeScore*50,timeScore*50,featuresScore*50,ageScore*50,salaryScore*50]
+    return {
+      baifenshu : postion,
+      arr : [degreeScore*50,timeScore*50,featuresScore*50,ageScore*50,salaryScore*50]
+    } 
+  },
+
+  dateNum(dateStr){
+    const year = dateStr.slice(0, dateStr.indexOf("年"));
+    const month = dateStr.slice(dateStr.indexOf("年") + 1, dateStr.indexOf("月"));
+    // 使用 Date 对象获取当前年、月和日期
+  const currentDate = new Date();
+  const nowyear = currentDate.getFullYear();
+  const nowmonth = currentDate.getMonth() + 1;  // 月份从 0 开始，所以需要加 1
+
+
+  // console.log(year-nowyear);  // 输出当前年份
+  // console.log(month-nowmonth);  // 输出当前月份
+    // 输出当前日期
+  return month-nowmonth;
   },
 
   onLoad() {
+  
     // your onReady logic here
     // app.globalData.analyesName=this.data.userName;
+    
   }
 
 });
